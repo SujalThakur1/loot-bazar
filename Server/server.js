d51f774564c1b8984d946dd07a9bb2f1d80a2231
SUJAL THAKUR <114414537+SujalThakur1@users.noreply.github.com>
2024-08-06 11:46:11 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://loot-bazar-frontend.vercel.app",
     credentials: true,
   })
 );
