9606a936ef617112d2cca801bb9499c4e289e7de
SUJAL THAKUR <114414537+SujalThakur1@users.noreply.github.com>
2024-08-06 17:04:15 +0100

    Update Register.jsx

--- Client/src/Pages/Register/Register.jsx
+++ Client/src/Pages/Register/Register.jsx
@@ -21,7 +21,7 @@ export default function Login() {
       } else {
         setError("");
         await axios.post(
-          "http://localhost:3000/user/register",
+          "https://loot-bazar-lwt5xh0ir-sujal-thakurs-projects-ec0a914c.vercel.app",
           { ...user },
           { withCredentials: true }
         );
