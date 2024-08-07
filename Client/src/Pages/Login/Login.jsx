18c8fb691c8b69c8fd0db29dce47fc914d2d9f27
SUJAL THAKUR <114414537+SujalThakur1@users.noreply.github.com>
2024-08-07 07:05:35 +0100

    Update Login.jsx

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
       } else {
         setError("");
         await axios.post(
-          "https://loot-bazar-lwt5xh0ir-sujal-thakurs-projects-ec0a914c.vercel.app/user/login",
+          "https://loot-bazar-api.onrender.com/user/login",
           {
             ...user,
           },
