e4a6c86f12e41c5d4f6f5ec8fb4abc551b63d251
SUJAL THAKUR <114414537+SujalThakur1@users.noreply.github.com>
2024-08-07 07:03:44 +0100

    Update ProductApi.jsx

--- Client/src/Api/ProductApi.jsx
+++ Client/src/Api/ProductApi.jsx
@@ -6,7 +6,7 @@ export default function useProductApi() {
 
   const getProducts = async () => {
     try {
-      const res = await axios.get("https://loot-bazar-lwt5xh0ir-sujal-thakurs-projects-ec0a914c.vercel.app/api/product");
+      const res = await axios.get("https://loot-bazar-api.onrender.com/api/product");
       setProducts(res.data.Products);
     } catch (error) {
       console.error("Error fetching product data:", error);
